abeda915075ac38790643445d411e183b7e9cd57
RanethM <raneth.20240766@iit.ac.lk>
2026-01-04 12:45:42 +0530

    made changes to the deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
+
+  base: '/Deluxe-Estates/',
+
   test: {
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
