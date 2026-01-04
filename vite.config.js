078882462b4a4066a0b7e88d74b4bea0b39f6e26
RanethM <raneth.20240766@iit.ac.lk>
2026-01-05 00:57:39 +0530

    Prepare for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/Deluxe-Estates/",
+  base: "/",
   plugins: [react()],
 
   test: {
