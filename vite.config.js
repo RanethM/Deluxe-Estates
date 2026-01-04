6a07c95b17a9cf553257c5a2d5a95e8de666baf9
RanethM <raneth.20240766@iit.ac.lk>
2026-01-04 14:38:15 +0530

    Set homepage and verify Vite base for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Deluxe-Estates/',
+  base: "/Deluxe-Estates/",
   plugins: [react()],
 
   test: {
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
     globals: true
   },
 });
