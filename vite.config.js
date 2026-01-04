3d9f423c7ab40362957ae877904e650d984d007e
RanethM <raneth.20240766@iit.ac.lk>
2026-01-04 14:24:23 +0530

    final commit before deployment

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-
-
   base: '/Deluxe-Estates/',
+  plugins: [react()],
 
   test: {
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
     globals: true
-  }
+  },
 });
